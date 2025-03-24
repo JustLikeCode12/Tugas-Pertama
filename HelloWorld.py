21a14c3f8b234deba2731accf72ce730508f2d90
JustLikeCode12 <akmalrizz123@gmail.com>
2025-03-24 11:27:22 +0700

    HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 for i in range(1001):
+=======
+for i in range(1001):
+>>>>>>> 29ad782 (HelloWorld.py)
     print("{}. hello world".format(i))
--- HelloWorld1.py
+++ HelloWorld1.py
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 i = 1
 while i<= 1000:
     print("{}. Hello world".format(i))
+=======
+i = 1
+while i<= 1000:
+    print("{}. Hello world".format(i))
+>>>>>>> 29ad782 (HelloWorld.py)
     i+=1
